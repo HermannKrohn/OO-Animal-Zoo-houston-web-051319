7f451980d9618201f42aa2cfcf7bb41df9e3c790
Hermann Krohn <krohnhermannacosta@gmail.com>
2019-05-15 16:53:22 -0500

    Done.

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,3 +1,37 @@
 class Animal
+    attr_reader :species, :nickname
+    attr_accessor :zoo
+
+    @@all = []
+    def initialize(species, weight, nickname)
+        @species = species
+        @nickname = nickname
+        if weight > 0
+            @weight = weight
+        else
+            @weight = 1
+        end
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def weight
+        @@weight
+    end
+
+    def weight=(value)
+        if value > 0
+            @@weight = value
+        end
+    end
+
+    def self.find_by_species(species)
+        self.all.select do |animalObject|
+            animalObject.species == species
+        end
+    end
 
 end
--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,3 +1,53 @@
 class Zoo
+attr_reader :name, :location
 
+@@all = []
+    def initialize(name, location)
+        @name = name
+        @location = location
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def animals
+        Animal.all.select do |animalObject|
+            animalObject.zoo == self.name
+        end
+    end
+
+    def animal_species
+        speciesArr = []
+        animalInZoo = self.animals
+        uniqObjectArr = animalInZoo.uniq do |animalObject|
+            animalObject.species
+        end
+
+        uniqObjectArr.each do |object|
+            speciesArr << object.species
+        end
+        speciesArr
+    end
+
+    def find_by_species(species)
+        animalInZoo = self.animals
+        animalInZoo.select do |animal|
+            animal.species == species
+        end
+    end
+
+    def animal_nicknames
+        animalInZoo = self.animals
+        animalInZoo.map do |animalObject|
+            animalObject.nickname 
+        end
+    end
+
+    def self.find_by_location(location)
+        self.all.select do |zooObject|
+            zooObject.location == location
+        end
+    end
 end
